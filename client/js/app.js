276d6dba77f9f45f769067b3f4b7d91e73f34eec
Jim Cowart <jim@ifandelse.com>
2015-06-01 21:14:40 -0500

    Updated to latest lux, switched 6to5 to babel

--- client/js/app.js
+++ client/js/app.js
@@ -1,23 +1,23 @@
 define( [
-	"6to5/polyfill",
+	"babel/polyfill",
 	"react",
 	"lux.js",
 	"components/ViewComponent.jsx",
 	"data/remote",
 	"helpers/markAsRead"
 ], function( to5, React, lux, ViewComponent ) {
-		var appEl = document.querySelector( ".app" );
-		React.render( React.createElement( ViewComponent ), appEl );
+	var appEl = document.querySelector( ".app" );
+	React.render( React.createElement( ViewComponent ), appEl );
 
-		var boot = lux.actionCreatorListener( {
-			handlers: {
-				pageInitialized: function() {
-					this.publishAction( "loadMessages" );
-				}
+	var boot = lux.actionCreatorListener( {
+		handlers: {
+			pageInitialized: function() {
+				this.publishAction( "loadMessages" );
 			}
-		} );
-
-		boot.publishAction( "loadPage" );
-
-		window.lux = lux;
+		}
 	} );
+
+	boot.publishAction( "loadPage" );
+
+	window.lux = lux;
+} );
